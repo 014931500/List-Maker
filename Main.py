f9ea8fb275f2aa8a194d36fa529fa666e0390c6a
Muath Al-onzi <108603693+014931500@users.noreply.github.com>
2024-01-18 22:10:41 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,10 +4,12 @@ letters = str(input("Press Enter Letters ? : "))
 num = int(input("Press Enter Number :"))
 domin = open("domains.txt",'r').read().splitlines()
 
+
 for Enter_Email in domin:
-    email= ''.join(random.choice(letters) for i in range(num))+ str(Enter_Email)
-    if email[0] in string.digits:
-        ''
-    else:
-        print(email+'\n')
-        open('List-emails.txt','a').write(f'{email}\n')
+    while 1:
+        email= ''.join(random.choice(letters) for i in range(num))+ str(Enter_Email)
+        if email[0] in string.digits:
+            ''
+        else:
+            print(email+'\n')
+            open('List-emails.txt','a').write(f'{email}\n')
